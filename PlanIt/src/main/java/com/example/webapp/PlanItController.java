2cfc4c4b62b79c2bb127fcfcadc9d8afb194447f
ChrisCross007 <58778210+ChrisCross007@users.noreply.github.com>
2020-02-13 16:16:23 +0100

    linking name to password

--- PlanIt/src/main/java/com/example/webapp/PlanItController.java
+++ PlanIt/src/main/java/com/example/webapp/PlanItController.java
@@ -4,18 +4,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 
 public class PlanItController {
 
     @GetMapping("/login")
-    public String showLoginSite(Model m){
+    public String showLoginSite(){
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(Model m, HttpSession session, @RequestParam String username, @RequestParam String password, HttpSession s){
+        if (username.equals("admin") && password.equals("admin")){
+            session.setAttribute("logger",username);
+            return "dash";
+        }
+        return "login";
+
+
     }
 
     @GetMapping("/register")
     public String showRegister(){
         return "register";
+    }
+
+    @GetMapping("/dashboard")
+    public String showDash(){
+        return "dash";
     }
 }
