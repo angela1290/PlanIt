53527fd1adee584f620dabd4af5bda1a341ea3d5
Angela <angela.jakobsen@gmail.com>
2020-02-14 11:22:43 +0100

    Oppdatert register.html

--- PlanIt/src/main/java/com/example/webapp/RegisterController.java
+++ PlanIt/src/main/java/com/example/webapp/RegisterController.java
@@ -5,10 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class RegisterController {
@@ -28,7 +25,7 @@ public class RegisterController {
     public String registerNewUser(Model m, @ModelAttribute User user){
 
        allUsers.addNewUser(user);
-       
+
         m.addAttribute("allUser", allUsers.getAllUsers());
 
         return "register";
